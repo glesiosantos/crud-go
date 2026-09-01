565ce3c91a6ab19fac3f80674a774c10e5bba36f
Glêsio Santos <glesioss@gmail.com>
2026-09-01 09:26:17 -0300

    movendo conexão do banco para infra

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,17 +1,30 @@
 package main
 
 import (
-	"context"
 	"log"
-	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+	"os"
 	"github.com/go-chi/chi/v5"
 	"crud-go/internal/cliente"
+	"crud-go/infra/database"
 	"net/http"
 )
 
 func main() {
-	url := "postgres://postgres:102030@localhost:5432/cruddb"
-	db, err := pgxpool.New(context.Background(), url)
+
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Erro ao carregar arquivo .env")
+	}
+
+	url := os.Getenv("DATABASE_URL")
+
+	if url == "" {
+		log.Fatal("DATABASE_URL não foi definida")
+	}
+
+	db, err := database.NewPostgresPool(url)
 
 	if err != nil {
 		panic("Erro ao conectar")
